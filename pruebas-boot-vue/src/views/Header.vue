5c9eab6bcf22233d7b3544340263b7ae58353313
muakarada <keymoca@hotmail.com>
2021-10-12 16:12:54 -0500

    Otras pruebas para página

---
+++ pruebas-boot-vue/src/views/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="header">
+        <div class="container">
+            <!--Barra de navegación-->
+            <b-navbar class="navBibliotech" toggleable="md">
+                <a ref="">Inicio</a>
+                <b-navbar-toggle target="navbar-toggle-collapse">
+                <template #default="{ expanded }">
+                    <b-icon v-if="expanded" icon="chevron-bar-up"></b-icon>
+                    <b-icon v-else icon="chevron-bar-down"></b-icon>
+                </template>
+                </b-navbar-toggle>
+            </b-navbar>
+        </div>
+    </div>
+
+</template>
+
+<style>
+    .navBibliotech{
+        background-color: red;
+    }
+
+</style>
